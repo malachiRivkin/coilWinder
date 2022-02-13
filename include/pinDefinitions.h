eb3d5f5281e3bf80c1999c171e9aa0620e9b96a3
Malachi <malachiriv@gmail.com>
2022-02-12 16:52:41 -0800

    IO

---
+++ include/pinDefinitions.h
@@ -0,0 +1,15 @@
+#define aAxis0 = 0;
+#define aAxis1 = 1;
+#define aAxis2 = 2;
+#define aAxis3 = 3;
+
+#define xAxis0 = 4;
+#define xAxis0 = 5;
+#define xAxis0 = 6;
+#define xAxis0 = 7;
+
+#define xPlus = 8;
+#define xMinus = 9;
+#define origin = 10;
+#define cycleStart = 11;
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,61 @@
 #include <Arduino.h>
+#include <pinDefinitions.h>
+
+#include <Bounce2.h>
+
+
+
+bool Calibrated = FALSE;
+
+ //instantiate debounce buttons
+  Bounce originB = Bounce();
+  Bounce jogMinus = Bounce();
+  Bounce jogPlus = Bounce();
+  Bounce cycleStartB = Bounce();
+  
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  //setup buttons
+  originB.attach( BOUNCE_PIN ,  INPUT_PULLUP ); // USE INTERNAL PULL-UP
+  originB.interval(5); // interval in ms
+
+  jogMinus.attach( BOUNCE_PIN ,  INPUT_PULLUP ); // USE INTERNAL PULL-UP
+  jogMinus.interval(5); // interval in ms
+
+  jogPlus.attach( BOUNCE_PIN ,  INPUT_PULLUP ); // USE INTERNAL PULL-UP
+  jogPlus.interval(5); // interval in ms
+
+  cycleStartB.attach( BOUNCE_PIN ,  INPUT_PULLUP ); // USE INTERNAL PULL-UP
+  cycleStartB.interval(5); // interval in ms
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  //which mode are we in?
+
+  //calibrate
+  //jog x+/- with UI, set origin
+  calibrate();
+
+  //get design instructions
+  //what is coil pitch, in inches?
+  //how many turns??
+  //which direction to translate?
+
+  //confirm design instructions
+  // - display settings
+  // - are they correct? -> "cycle start"
+
+  //execute design instructions
+  //turn A,X axis simultaneously
 }
+
+
+void calibrate(){
+  Serial.println("Calibration Mode: Set X Origin")
+  while(!Calibrated){
+    originB.update();
+
+  }
+}
+
